b1c0011a068818d4193d68ac1c0b10aa1de9f9d0
Natalia Nikolaeva <n.nikolaeva@gmail.com>
2018-10-06 16:00:11 -0700

    First commit: new react app

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const posts = [
+  {"id": 1, "content": "test", "timestamp": 1537811000, "priority": 1, "user": {"name": "nn"}},
+  {"id": 2, "content": "test2", "timestamp": 1537811001, "priority": 1, "user": {"name": "mm"}},
+  {"id": 3, "content": "test3", "timestamp": 1537811002, "priority": 1, "user": {"name": "pp"}}
+];
+
 class App extends Component {
+  state = {
+    posts: posts
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>nav</p>
         </header>
+        <div className="main-container">
+        
+        </div>
+
+
       </div>
     );
   }
